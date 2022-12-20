d740dea8300cfde37a137f28a6800807a0d3c455
kkaoww <120167981+kkaoww@users.noreply.github.com>
2022-12-20 16:10:16 +0700

    Update lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,19 +1,34 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    int numK = x;
+    char text = numK ;
+    if(numK <= 91 && numK >48)
+    {
+        numK--;
+        if(numK < 65)
+        {
+            numK += 26;
+        }
+    }
+    else
+    {
+        numK = 48;
+    }
+    text = numK;
+    return text;
 }
-
 int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    cout << before('A') << "\n";
+    cout << before('B') << "\n";
+    cout << before('P') << "\n";
+    cout << before('T') << "\n";
+    cout << before('Z') << "\n";
+    cout << before('a') << "\n";
+    cout << before('0') << "\n";
+    cout << before('c') << "\n";
+    return 0;
 }
